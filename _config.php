f0299ea1cb9daf144e958419782be5688a68158a
Nathan Glasl <nathan@silverstripe.com.au>
2014-11-24 10:07:57 +1100

    Updating the composer requirements.

--- _config.php
+++ _config.php
@@ -5,9 +5,6 @@
  *	@author Nathan Glasl <nathan@silverstripe.com.au>
  */
 
-if(!class_exists('MultiValueField')) {
-	exit('The <strong>extensible search</strong> module requires the <strong>multivalue field</strong> module.');
-}
 if(!defined('EXTENSIBLE_SEARCH_PATH')) {
 	define('EXTENSIBLE_SEARCH_PATH', rtrim(basename(dirname(__FILE__))));
 }
