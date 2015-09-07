7c90f6fe6cf39fc9e613e8d82ef66005c837cda7
Stephen McMahon <stephen@silverstripe.com.au>
2015-09-07 18:03:42 +1000

    Add PageStore local storage code to typeahead.js

--- javascript/extensible-search-typeahead.js
+++ javascript/extensible-search-typeahead.js
@@ -1,14 +1,14 @@
 ;(function($) {
+	window.ESPPageStore = new PageStore;
 	$(window).load(function() {
 
-		// Bind autocomplete to the search form.
 
 		var search = $('input.extensible-search.typeahead');
 		var resentsearch = $('.recentsearch').first().clone();//grab a rescent search list item for cloning
 		$('.recentsearch').remove();//remove any blank ones
 		$('.recent-searches-list').each(function(){
 			var list = $(this);
-			$.each(DpcLocalStore.getType('SearchTerms'), function (i, term) {
+			$.each(ESPPageStore.getType('SearchTerms'), function (i, term) {
 				list.prepend(resentsearch.clone().children('a').prop('href',term.link).text(term.title).parent());
 			});
 		});
@@ -57,4 +57,83 @@
 		}
 
 	});
+
+	/*
+	 * Page Store
+	 * Uses local storage to track search pages that have links clicked on them. This helps users allowing us to
+	 * return recent searches the users has made and found useful
+	 */
+	var bindSearchCapture = function () {
+		$(document).on('click', 'div.search-result a', function (e) {
+			var url = location.pathname + location.search;
+			var searchTerm = $('#SearchForm_getForm_Search').val();
+			var item = {
+				link: url,
+				title: searchTerm,
+				type: 'SearchTerms',
+				class: ''
+			}
+			DpcLocalStore.recordView(item);
+		});
+	};
+
+	function PageStore() {
+		this.items = {'all': []};
+		this.numberToStore = 5;
+
+		if (window.localStorage) {
+			var rawData = window.localStorage.getItem('pageview');
+			if (rawData && rawData.length) {
+				this.items = JSON.parse(rawData);
+			}
+		}
+	}
+
+	PageStore.prototype.empty = function () {
+		if (window.localStorage) {
+			window.localStorage.clear();
+			this.items = {'all': []};
+		}
+	}
+
+	PageStore.prototype.getType = function (cat) {
+		if (!this.items[cat]) {
+			this.items[cat] = [];
+		}
+		return this.items[cat];
+	}
+
+	PageStore.prototype.recordView = function (pageItem) {
+		if (pageItem.type && pageItem.type.length) {
+			var addTo = this.getType(pageItem.type);
+
+			for (var i in addTo) {
+				if (!addTo[i].link) {
+					addTo.splice(i, 1);
+					continue;
+				}
+				if (addTo[i].link == pageItem.link) {
+					addTo.splice(i, 1);
+				}
+			}
+
+			addTo.unshift({
+				'link': pageItem.link,
+				'class': pageItem.classes,
+				'title': pageItem.title
+			});
+			if (addTo.length > this.numberToStore) {
+				addTo.pop();
+			}
+
+			this.save();
+		}
+	}
+
+	PageStore.prototype.save = function () {
+		if (window.localStorage) {
+			window.localStorage.setItem('pageview', JSON.stringify(this.items));
+		}
+	}
+
 })(jQuery);
