ad66c6063353f7b074011ce9a7ea362c651ed890
Nathan Glasl <nathan@silverstripe.com.au>
2016-01-18 12:42:37 +1100

    Updating the extensible search page image.

--- _config.php
+++ _config.php
@@ -8,3 +8,7 @@
 if(!defined('EXTENSIBLE_SEARCH_PATH')) {
 	define('EXTENSIBLE_SEARCH_PATH', rtrim(basename(dirname(__FILE__))));
 }
+
+// Update the current extensible search page image.
+
+Config::inst()->update('ExtensibleSearchPage', 'icon', EXTENSIBLE_SEARCH_PATH . '/images/search.png');
